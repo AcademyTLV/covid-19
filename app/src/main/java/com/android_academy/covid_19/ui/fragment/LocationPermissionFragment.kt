b557d1bf0d3334ebf2d0467cb788208b2de20987
Sagi <Sagi363@gmail.com>
2020-03-16 12:08:46 +0200

    Premission location dialog UI + Material components

--- app/src/main/java/com/android_academy/covid_19/ui/fragment/LocationPermissionFragment.kt
+++ app/src/main/java/com/android_academy/covid_19/ui/fragment/LocationPermissionFragment.kt
@@ -1,5 +1,7 @@
 package com.android_academy.covid_19.ui.fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,10 @@ import com.android_academy.covid_19.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class LocationPermissionFragment: BottomSheetDialogFragment() {
+
+    val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                                            Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+    val REQUEST_LOCATION = 100
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,14 +26,20 @@ class LocationPermissionFragment: BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+        requestPermissions(permissions, REQUEST_LOCATION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
     }
 
     private fun initView() {
-        // btnLogin.setOnClickListener {
-        //     // viewModel.onLoginClick()
-        //     val intent = Intent(context, MapsActivity::class.java)
-        //     startActivity(intent)
-        // }
+
     }
 
     companion object {
