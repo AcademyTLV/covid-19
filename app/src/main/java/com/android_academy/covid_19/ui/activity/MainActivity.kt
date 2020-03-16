0db6b7ae9d5bbd6652a7d99cde1159d44e3ce4f2
Omer Tzadiki <omertzadiki@MacBook-Pro-6.local>
2020-03-16 13:29:04 +0200

    arragne code

--- app/src/main/java/com/android_academy/covid_19/ui/activity/MainActivity.kt
+++ app/src/main/java/com/android_academy/covid_19/ui/activity/MainActivity.kt
@@ -120,8 +120,9 @@ class MainActivity : AppCompatActivity() {
     private fun initViews(savedInstanceState: Bundle?) {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, IntroFragment.newInstance())
-                //.replace(R.id.container, UsersLocationListFragment())
+                // toggle to test intro fragment
+                //.replace(R.id.container, IntroFragment.newInstance())
+                .replace(R.id.container, UsersLocationListFragment())
                 .commitNow()
         }
     }
